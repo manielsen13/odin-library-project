2a2347d4af2c84675d5fbc137262fe4a9f8e77fb
manielsen13 <marshall.niels@gmail.com>
2022-10-18 13:39:07 -0400

    Create Files. Add Basic HTML Structures to Files. Link Script and Stylesheet to HTML File. Begin writing JS.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-library-project
+
+This is a project for TOP, where I create a simple Library app to practice using Objects.
---
+++ script.js
@@ -0,0 +1,29 @@
+
+let myLibrary = [];
+
+function book(title, author, numPages, haveRead) {
+    this.title = title;
+    this.author = author
+    this.numPages = numPages;
+    this.haveRead = haveRead;
+
+    this.getInfo = function() {
+        return (this.title + " by " + this.author + ", " + numPages + " pages," + " " + this.haveRead);
+    }
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayBooks(){
+    for (let i = 0; i < myLibrary.length; i++) {
+        //create a card here that displays all of the information for that book.
+    }
+
+
+
+
+}
+
+const b1 = new book("The Hunger Games", "Collins", 312, "read");
